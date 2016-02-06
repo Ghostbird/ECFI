e6c11a26a00ddf64c2e840c2666cd7facc0ba764
Ghostbird <gijsbertth@gmail.com>
2016-02-06 14:26:11 +0100

    Generated documentation for changes in previous commit.

--- doc/html/search/all_2.js
+++ doc/html/search/all_2.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['rb_5fcreate',['rb_create',['../ringbuffer_8c.html#aac36d182ea8286d62440474f799a125d',1,'ringbuffer.c']]],
   ['rb_5fdestroy',['rb_destroy',['../ringbuffer_8c.html#adc1743cdff70bf57fa6e23416e849bab',1,'ringbuffer.c']]],
-  ['rb_5fread',['rb_read',['../ringbuffer_8c.html#a9ba337ef9e96abead4409889567d85fc',1,'ringbuffer.c']]],
+  ['rb_5fread',['rb_read',['../ringbuffer_8c.html#a387b9d6b5a464188b47d56472531fea3',1,'ringbuffer.c']]],
   ['read',['read',['../structringbuffer.html#aab46f34796dcd9dce069ca831c928dec',1,'ringbuffer']]],
   ['ringbuffer',['ringbuffer',['../structringbuffer.html',1,'']]],
   ['ringbuffer_2ec',['ringbuffer.c',['../ringbuffer_8c.html',1,'']]]
--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['rb_5fcreate',['rb_create',['../ringbuffer_8c.html#aac36d182ea8286d62440474f799a125d',1,'ringbuffer.c']]],
   ['rb_5fdestroy',['rb_destroy',['../ringbuffer_8c.html#adc1743cdff70bf57fa6e23416e849bab',1,'ringbuffer.c']]],
-  ['rb_5fread',['rb_read',['../ringbuffer_8c.html#a9ba337ef9e96abead4409889567d85fc',1,'ringbuffer.c']]]
+  ['rb_5fread',['rb_read',['../ringbuffer_8c.html#a387b9d6b5a464188b47d56472531fea3',1,'ringbuffer.c']]]
 ];
