2ad93730be5c6ec09ea5c96643cd22adb12a1e1c
Ghostbird <gijsbertth@gmail.com>
2016-06-23 14:39:25 +0200

    Multiple changes, see details
    Write generated files to the _cfg_ directory
    Generate files as SVG instead of PNG
    Write generated dot file to disk

--- cfggen.py
+++ cfggen.py
@@ -5,8 +5,12 @@ Created on Tue Jun 22 14:37:04 2016
 
 """
 import gcc
+import os
 from gccutils import get_src_for_loc, cfg_to_dot
 from subprocess import Popen, PIPE
+
+CDIR = 'cfg'
+FORMAT = 'svg'
 
 class ShowGimple(gcc.GimplePass):
     def execute(self, fun):
@@ -14,7 +18,12 @@ class ShowGimple(gcc.GimplePass):
         # in SSA form)
         if fun and fun.cfg:
             dot = cfg_to_dot(fun.cfg, fun.decl.name)
-            Popen(['dot', '-Tsvg', '-o', fun.decl.name], stdin=PIPE)
+            filename = os.path.join(CDIR, fun.decl.name)
+            with open(filename + '.dot','w') as dotfile:
+                dotfile.write(dot)
+                print('Wrote dot file: {}.dot'.format(filename))
+            Popen(['dot', '-T' + FORMAT, '-o', '{}.{}'.format(filename, FORMAT)], stdin=PIPE).communicate(dot.encode('utf-8'))
+            print('Wrote: {}.{}\n'.format(filename,FORMAT))
 
 ps = ShowGimple(name='show-gimple')
 ps.register_after('cfg')
