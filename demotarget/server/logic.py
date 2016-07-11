08e6bd5041df546a230a51efa70e8eb897ed9c06
bl4ckic3 <al13@mails.tsinghua.edu.cn>
2016-07-11 12:02:02 +0200

    Remove

--- demotarget/server/logic.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""
-DummyModbusSVC server (slave) logic component
-
-Simple dummy modbus TCP service logic, reads contents of coil/bit/register state as saved by frontend daemon.
-
-TODO: implement
-"""
