a7dd1b523b794a26786a82897847ef0b5ca3b9e9
Ghostbird <gijsbertth@gmail.com>
2016-07-18 22:09:48 +0200

    Fix for missing newlines and use of spaces instead of tabs.

--- injectioncode/Call-C-Function-Epilogue.s
+++ injectioncode/Call-C-Function-Epilogue.s
@@ -1,4 +1,4 @@
-        LDR R0, =HOTSITEIDHERE @if hotsiteid is less than 1024 use MOV instead.
-        LDR R1, LR
+        LDR	R0, =HOTSITEIDHERE @if hotsiteid is less than 1024 use MOV instead.
+        LDR	R1, LR
         LDR     r3, =rb_write_attached
         BLX     r3
