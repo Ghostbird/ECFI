f211ea4f1b3dbb81f3f6a4e6f26597e7ad60a784
Ali Abbasi <a.abbasi@utwente.nl>
2016-07-18 03:30:55 +0200

    Added dynamic function call based injection code

---
+++ injectioncode/Call-C-Function-IndirectBranch.s
@@ -0,0 +1,6 @@
+        PUSH    {r0, r1, r3}
+        LDR R0, HOTSITEID
+        LDR r1, [FORWARDEDGELOCATION]
+        LDR     r3, =rb_write_attached
+        BLX     r3
+        POP    {r0, r1, r3}
