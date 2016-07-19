092cb638ea171b1d457891ed02ad40492fe6e206
Ali <bl4ckic3@users.noreply.github.com>
2016-07-19 20:17:18 +0200

    Update dynamicpushpopinjectioncode.s

--- injectioncode/dynamicpushpopinjectioncode.s
+++ injectioncode/dynamicpushpopinjectioncode.s
@@ -5,7 +5,7 @@
         SUB     r6, r5, #8  @calculating ringbuffer write offset address. Avoding memory pool
         LDR     r6, [r6]        @load the value of write offset
         ADD     r5, r6          @add the W+Buff
-        LDR     r6,[lr]
+        MOV     r6,LR
         STMIA   r5!, {r8,r6}   @push the values of dereferenced LR and HOTSITEID(R6) to RB and increment the r5 with 8 bytes
         LDR     r6, =[rb_writer_end]      @Load the End of buffer address
         LDR     r6, [r6]
