a0771867e9c5e7ec11216b17171a57a2f988038e
Gijsbert ter Horst <gijsbertth@gmail.com>
2016-07-21 15:27:21 +0200

    Update pointer for debugging with new version of cfi-checker.

--- gdb.sh
+++ gdb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # One monstrous command to make GDB end up exactly at the main of the exectuable passed on the command line, running in bin/cfi-checker.
-gdb -ex "set env LD_PRELOAD bin/lib/libringbuffer.so" -ex "file bin/cfi-checker" -ex "b main" -ex "b *0x00011108" -ex "run $@" # -ex "continue" -ex "set follow-fork-mode child" -ex "continue"
+gdb -ex "set env LD_PRELOAD bin/lib/libringbuffer.so" -ex "file bin/cfi-checker" -ex "b main" -ex "b *0x000111e4" -ex "run $@" # -ex "continue" -ex "set follow-fork-mode child" -ex "continue"
