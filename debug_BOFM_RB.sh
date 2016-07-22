8a93df0f1051c895592c8be17d2147b81a79d903
Gijsbert ter Horst <gijsbertth@gmail.com>
2016-07-22 20:33:04 +0200

    Delete test buffer. Add test script.

---
+++ debug_BOFM_RB.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+make DEBUG=-ggdb bin/cfi-checker
+# Check if target needs to be remade.
+make -q asm/BOFM_RB.s
+remake=$?
+if [ $remake -eq 1 ]:
+then
+    make asm/BOFM_RB.s
+    python3 injector.py asm/BOFM_RB.s dot/BOFM_RB.dot asm/BOFM_RBi.s
+    make DEBUG=-ggdb asm_to_bin/BOFM_RBi
+fi
+rm /dev/shm/rb_cfi_BOFM_RBi
+./gdb.sh asm_to_bin/BOFM_RBi cfg/BOFM_RB.bin
